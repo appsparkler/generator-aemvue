a1599523469b3fd180843fb3f9a14c1796d9cdfa
Akash <ashah10@sapient.com>
2018-12-17 06:49:27 +0530

    duplicated template-create for template-delete

---
+++ generators/template-delete/templates/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+div.vue-template-component.<%= template.templateName %>-template
+  pre.bg-primary.text-white.m-5.p-5.text-center
+    h1 <%= template.title %>
+    p <%= template.description %>
+</template>
+
+<script>
+import <%= template.templateName %>Config from "./config";
+//
+export default {
+  data() {
+    return {
+      config: <%= template.templateName %>Config
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
