8d2b780ab86db8b4eaa1c8b8773150521d7ea545
Akash <subs@appsparkler.com>
2019-06-04 14:15:10 +0530

    WIP

--- generators/experience/GET_PROMPTS.js
+++ generators/experience/GET_PROMPTS.js
@@ -1,66 +1,88 @@
 /*eslint-disable*/
 module.exports = function() {
-  const {templateName} = this.options;
-  const YoRC = this.config.getAll();
-  const templateContainerConfig = YoRC.templateContainer;
-  //
+  const changeCase = require('change-case');
   const PROMPTS = [
-    // template.templateName
+    //
     {
       type: "input",
-      name: "template.templateName",
-      message: "What is the name for this template (the template-folder-name)?",
-      default: `${templateName}`
+      name: "xp.component.category",
+      message: "Please enter the category under which you would like to create this experience: ",
+      required: true
     },
-    // template.title
     {
+        type: "input",
+        name: "xp.component.name",
+        message: "Please enter the name of your experience (this will be converted to camelCase) (examples: imageLink, textLink, carousel): ",
+        required: true,
+        filter: (userInput) => changeCase.camel(userInput)
+    },
+    {
+        type: "input",
+        name: "xp.component.title",
+        message: "Please update a title for your experience (Ex. Navbar, Footer..): ",
+        required: true
+    },
+    {
+        type: "input",
+        name: "xp.component.group",
+        message: "Please select the component group for this component: ",
+        required: true
+    },
+    // template.templateName
+  /*  {
+      type: "input",
+      name: "app.pathToAEMProject",
+      message: "Please enter the path to the AEM Project folder."
+    }, */
+    // template.title
+/*    {
       type: "input",
       name: "template.title",
       message: "What is the title for this template?",
       default: `${templateName} Template`
-    },
+    }, */
     // template.description
-    {
+/*    {
       type: "input",
       name: "template.description",
       message: "Please describe this template.",
       default(answers) {
         return `A template for ${answers.template.title}.`;
       }
-    },
+    }, */
     // template.category (category)
-    {
+/*    {
       type: "list",
       name: "template.category",
       message: "Please provide template sub-folder?",
       choices: ["global", "content"],
       default: "global"
-    },
+    }, */
     // template.allowedPaths
-    {
+/*    {
       type: "input",
       name: "template.allowedPaths",
       message:
         "What paths are allowed to access this template (comma-seperated for multiple items)?",
       default: "/content/.+"
-    },
+    }, */
     // template.ranking
-    {
+/*    {
       type: "input",
       name: "template.ranking",
       message: "What is the ranking for this template?",
       default: 100
-    },
-    {
+    }, */
+/*    {
       type: "list",
       name: "template.slingType",
       message: "Please select sling-type for this CQ template:",
       choices: ["sling:resourceSuperType", "sling:resourceType"],
       default: "sling:resourceType"
-    },
+    }, */
     // template.resourcePath
     // template.slingType
-    {
+/*    {
       type: "input",
       name: "template.resourcePath",
       message:
@@ -77,25 +99,25 @@ module.exports = function() {
         }
         return resourcePath;
       }
-    },
+    }, */
     // COMPONENT PROMPTS
     // component.slingType
-    {
+/*    {
       type: "list",
       name: "component.slingType",
       message: "please select the sling-type for this template-component:",
       choices: ["sling:resourceSuperType", "sling:resourceType"],
       default: "sling:resourceSuperType"
-    },
+    }, */
     // component.resourceType (2 prompts to get answer)
-    {
+/*    {
       type: "list",
       name: "component.resourcePath",
       message: "please select the resourcePath for this component:",
       choices: templateContainerConfig.basePagePaths.concat(["others"]),
       default: templateContainerConfig.basePagePaths[1]
-    },
-    {
+    }, */
+/*    {
       when(answers) {
         if (answers.component.resourcePath === "others") {
           return true;
@@ -106,8 +128,26 @@ module.exports = function() {
       type: "input",
       name: "component.resourcePath",
       message: "please specify the resourcePath for this component:"
-    }
+    } */
   ];
 
   return PROMPTS;
 };
+
+// private functions
+function validate_pathToAEMProject(userInputPath) {
+  try {
+      const fs = require('fs-extra');
+      const path = require('path');
+      const uiAppDirPath = path.resolve(userInputPath, 'ui.apps');
+      const errMsgs = {
+        invalidDir: "The directory is not valid",
+        missingUIAppsDirs: "This directory doesn't have a ui.apps folder"
+      };
+      if(!fs.existsSync(userInputPath)) return errMsgs.invalidDir;
+      else if(!fs.existsSync(uiAppDirPath)) return errMsgs.missingUIAppsDirs;
+      else return true;
+  } catch (e) {
+      console.log(e);
+  }
+}
---
+++ generators/experience/index.js
@@ -0,0 +1,190 @@
+/* eslint-disable*/
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const GET_PROMPTS = require("./GET_PROMPTS");
+
+module.exports = class extends Generator {
+
+  initializing() {
+    welcome_theUser.call(this);
+  }
+
+  async prompting() {
+    await set_AnswersForPrompts.call(this);
+  }
+
+  configuring() {
+    // this.destinationRoot(`${this.answers.app.name}-webapp`);
+    // set_config.call(this);
+  }
+
+  default() {}
+
+  writing() {
+    // scaffold_app.call(this);
+  }
+
+  end() {
+    // conduct_userFarewell.call(this);
+  }
+};
+
+// private functions
+function set_config() {
+  this.config.set('appName', this.answers.app.name);
+  this.config.set(
+    'pathToAEMProjectFolder',
+    this.answers.app.pathToAEMProject
+  );
+  this.config.set("templates", {
+    "global": {
+      "BasePage": {
+        "name": "BasePage",
+        "title": "The Base Template Page",
+        "description": "This is the Base Template Page which loads all the vendor and common scripts along with the HTML document",
+        "chunks": [
+          "chunk-vendors",
+          "chunk-common",
+          "templates/global/BasePage/publishLibs"
+        ]
+      }
+    }
+  });
+  // YoRC.appName = this.answers.app.name;
+  // YoRC.pathToAEMProjectFolder = this.answers.app.pathToAEMProject;
+  // const templateName = this.answers.template.templateName;
+  // const category = this.answers.template.category;
+  // const templatePath = `templates/${category}/${templateName}`;
+  // const TemplateComponents = YoRC.templateContainer.TemplateComponents;
+  // TemplateComponents[templatePath] = templateName;
+  // YoRC.templateContainer.TemplateComponents = TemplateComponents;
+  // console.log(YoRC);
+  // this.config.set(YoRC);
+}
+
+function scaffold_app() {
+  const path = require('path');
+  const templatePaths = [
+    '/src/chunk-vendors/.content.xml',
+    '/src/chunk-common/.content.xml',
+    '/src/clientlibs/authoring/js/listeners/edtiable-added.js',
+    '/src/templates/global/BasePage/BasePage-cmp/customheaderlibs.html',
+    'package.json',
+    '/src/templates/global/BasePage/BasePage-cmp/customfooterlibs.html',
+    '/src/templates/global/BasePage/BasePage-tmpl/.content.xml',
+    '/src/templates/global/BasePage/publishLibs/.content.xml',
+    '/.env',
+    '/.env.development',
+    '/.env.production',
+    '/.gitignore'
+  ];
+  this.fs.copy(
+    this.templatePath("appName"),
+    this.destinationPath()
+  );
+  // Copy All DOT (.) files
+  this.fs.copy(
+    this.templatePath('appName/**/.*'),
+    this.destinationRoot()
+  );
+  templatePaths.forEach(key => {
+    this.fs.copyTpl(
+      this.templatePath(path.join('appName', key)),
+      path.join(this.destinationRoot(), key),
+      this
+    )
+  });
+  /*
+  this.fs.copyTpl(
+    this.templatePath(path.join('appName', filePaths.chunkCommonClientLib)),
+    this.destinationPath(filePaths.chunkCommonClientLib),
+    this
+  )
+  this.fs.copyTpl(
+    this.templatePath("appName", filePaths.editableAddedFile),
+    this.destinationPath(filePaths.editableAddedFile),
+    this
+  )
+  */
+}
+
+function welcome_theUser() {
+  this.log(
+    yosay(
+      ` Welcome to the
+        ${chalk.bgGreen.black(
+          "EXPERIENCE BUILDER..."
+        )}
+      `
+    )
+  );
+}
+
+function conduct_userFarewell() {
+  this.log(
+    yosay(
+      `Thank you for scaffolding your app with
+        ${chalk.bgGreen.black(
+          "AEM-VUE-GENERATOR"
+        )}
+        ...
+      `
+    )
+  );
+  this.spawnCommandSync('git', ['init']);
+  if(this.answers.ui.installNodeModules)  this.log('Please wait while we install your node_modules...');
+  if(this.answers.ui.installNodeModules) this.npmInstall();
+}
+
+async function set_AnswersForPrompts() {
+  const PROMPTS = GET_PROMPTS.call(this);
+  this.answers = await this.prompt(PROMPTS);
+  console.log(this.answers);
+}
+
+function copyTemplateJCR() {
+  let {category, templateName} = this.answers.template;
+
+  this.fs.copyTpl(
+    this.templatePath("aem-tmpl/_content.xml"),
+    this.destinationPath(
+      `./src/templates/${category}/${templateName}/aem-tmpl/.content.xml`
+    ),
+    this.answers
+  );
+}
+
+function copyComponentJCR() {
+  let {category, templateName} = this.answers.template;
+
+  this.fs.copyTpl(
+    this.templatePath("aem-component/_content.xml"),
+    this.destinationPath(
+      `./src/templates/${category}/${templateName}/aem-component/.content.xml`
+    ),
+    this.answers
+  );
+}
+
+function copyFolderJCR() {
+  const {category, templateName} = this.answers.template;
+  this.fs.copy(
+    this.templatePath("_content.xml"),
+    this.destinationPath(
+      `./src/templates/${category}/${templateName}/.content.xml`
+    )
+  );
+}
+
+/*
+	Initializing();
+	prompting();
+	configuring();
+	default();
+	writing();
+	conflicts();
+	install();
+	end();
+*/
