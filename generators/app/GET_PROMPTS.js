8b026121d9644af0406177813235f8ae68c1d407
Akash <subs@appsparkler.com>
2019-06-05 14:06:09 +0530

    Added default answer for appName

--- generators/app/GET_PROMPTS.js
+++ generators/app/GET_PROMPTS.js
@@ -1,131 +1,26 @@
 /*eslint-disable*/
 module.exports = function() {
-  // const { appName } = this.options;
-  // const YoRC = this.config.getAll();
-  // const templateContainerConfig = YoRC.templateContainer;
-  //
   const PROMPTS = [
     //
     {
-      type: "input",
-      name: "app.name",
-      message: "Please enter the name of your WebApp. The value for this should match with the Maven Archetype App you have scaffolded : ",
-      default: this.options.appName,
-      required: true
-    },
-    {
         type: "input",
         name: "app.pathToAEMProject",
-        message: "Please enter the path to the AEM Project folder: ",
+        message: "AEM project path?",
         validate: validate_pathToAEMProject.bind(this)
+    },
+    {
+      type: "input",
+      name: "app.name",
+      message: "App name?",
+      default: answers => answers.app.pathToAEMProject.match(/[a-z|A-Z]*?$/),
+      required: true
     },
     {
         type: "confirm",
         name: "ui.installNodeModules",
-        message: "Would you like to install the node modules afer scaffolding?",
+        message: "Install Node modules after scaffolding?",
         default: true
     }
-    // template.templateName
-  /*  {
-      type: "input",
-      name: "app.pathToAEMProject",
-      message: "Please enter the path to the AEM Project folder."
-    }, */
-    // template.title
-/*    {
-      type: "input",
-      name: "template.title",
-      message: "What is the title for this template?",
-      default: `${templateName} Template`
-    }, */
-    // template.description
-/*    {
-      type: "input",
-      name: "template.description",
-      message: "Please describe this template.",
-      default(answers) {
-        return `A template for ${answers.template.title}.`;
-      }
-    }, */
-    // template.category (category)
-/*    {
-      type: "list",
-      name: "template.category",
-      message: "Please provide template sub-folder?",
-      choices: ["global", "content"],
-      default: "global"
-    }, */
-    // template.allowedPaths
-/*    {
-      type: "input",
-      name: "template.allowedPaths",
-      message:
-        "What paths are allowed to access this template (comma-seperated for multiple items)?",
-      default: "/content/.+"
-    }, */
-    // template.ranking
-/*    {
-      type: "input",
-      name: "template.ranking",
-      message: "What is the ranking for this template?",
-      default: 100
-    }, */
-/*    {
-      type: "list",
-      name: "template.slingType",
-      message: "Please select sling-type for this CQ template:",
-      choices: ["sling:resourceSuperType", "sling:resourceType"],
-      default: "sling:resourceType"
-    }, */
-    // template.resourcePath
-    // template.slingType
-/*    {
-      type: "input",
-      name: "template.resourcePath",
-      message:
-        "template-component resource path : ",
-      default(answers) {
-        let resourcePath = "";
-        try {
-          const {appName} = YoRC;
-          const {category, templateName} = answers.template;
-          resourcePath =
-            `/apps/${appName}/src/templates/${category}/${templateName}/aem-component`;
-        } catch (e) {
-          console.error(e);
-        }
-        return resourcePath;
-      }
-    }, */
-    // COMPONENT PROMPTS
-    // component.slingType
-/*    {
-      type: "list",
-      name: "component.slingType",
-      message: "please select the sling-type for this template-component:",
-      choices: ["sling:resourceSuperType", "sling:resourceType"],
-      default: "sling:resourceSuperType"
-    }, */
-    // component.resourceType (2 prompts to get answer)
-/*    {
-      type: "list",
-      name: "component.resourcePath",
-      message: "please select the resourcePath for this component:",
-      choices: templateContainerConfig.basePagePaths.concat(["others"]),
-      default: templateContainerConfig.basePagePaths[1]
-    }, */
-/*    {
-      when(answers) {
-        if (answers.component.resourcePath === "others") {
-          return true;
-        } else {
-          return false;
-        }
-      },
-      type: "input",
-      name: "component.resourcePath",
-      message: "please specify the resourcePath for this component:"
-    } */
   ];
 
   return PROMPTS;
--- generators/app/index.js
+++ generators/app/index.js
@@ -6,13 +6,13 @@ const yosay = require("yosay");
 const GET_PROMPTS = require("./GET_PROMPTS");
 
 module.exports = class extends Generator {
-  constructor(args, opts) {
-    super(args, opts);
-    this.argument("appName", {
-      type: String,
-      required: true
-    });
-  }
+  // constructor(args, opts) {
+  //   super(args, opts);
+    // this.argument("appName", {
+    //   type: String,
+    //   required: true
+    // });
+  // }
 
   initializing() {
     welcome_theUser.call(this);
